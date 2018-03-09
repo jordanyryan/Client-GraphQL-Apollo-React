9cc3b65a390c1996fc7cbd9f501042a2a8d7f174
Jordany Rosas <jordanyryan@gmail.com>
2018-03-09 13:52:24 -0500

    Setup apollo provider and apollo client

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://lyrical-jd:jd1234@ds163418.mlab.com:63418/lyrical-db'
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
